ab07a781ec8de9e6124f4f0bb2654103ca8455d6
Pavel Filimonov <172885253+FilimonovPavel@users.noreply.github.com>
2024-08-06 23:31:51 +0300

    feat: create check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const brackets = new Map(bracketsConfig);
+  const openBrackets = new Set(bracketsConfig.map((el) => el[0]));
+  const closeBrackets = new Set(bracketsConfig.map((el) => el[1]));
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (brackets.get(char) === char && stack.includes(char)) {
+        if (stack.pop() !== char) {
+          return false;
+        }
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (brackets.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
